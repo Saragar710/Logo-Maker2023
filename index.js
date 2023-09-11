6ba34179f1132ffa04c7a2cf909ab7f37ff35eaa
Sara Garcia <saragca81@gmail.com>
2023-09-11 10:12:16 -0700

    added code to index

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-const inquirer = require('inquirer.js');
+const inquirer = require('inquirer');
 const fs = require('fs');
-const shapes = require('./shapes')
+const shapes = require('./lib/shapes')
 
 
-function Shape () {
-    constuctor( type, color, width, height, fill )
-    this.type = type;
-    this.color = color;
-    this.width = width;
-    this.height = height;
-    this.fill = fill;
+// function Shape () {
+//     constuctor( type, color, width, height, fill )
+//     this.type = type;
+//     this.color = color;
+//     this.width = width;
+//     this.height = height;
+//     this.fill = fill;
 
-    this.createShape = () => console.log (`This is my ${this.type} ${this.color}`)
+//     this.createShape = function ()  {console.log (`This is my ${this.type} ${this.color}`)}
+// }
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is your name?",
+    }
+]
+
+class Shape{
+    constructor(type, color, width, height, fill) {
+        this.type = type,
+        this.color = color,
+        this. width = width,
+        this.height = height,
+        this.fill = fill,
+        
+    };
+
+    showCreateShape() {
+        console.log(`The shape you choose is ${this.type} ${this.color} ${this.width} ${this.height} ${this.fill}`);
+    }
 }
+
+
+
 
 const circle = new Shape('circle', 'blue');
 const triangle = new Shape ('triangle', 'pink');
