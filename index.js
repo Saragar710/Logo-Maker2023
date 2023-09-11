8fbfa0d50ab30f191770bfddaf9ce23616ccd192
Sara Garcia <saragca81@gmail.com>
2023-09-11 09:11:48 -0700

    started code for project

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const inquirer = require('inquirer');
+const inquirer = require('inquirer.js');
+const fs = require('fs');
+const shapes = require('./shapes')
 
 
-function Shape (type, color) {
+function Shape () {
+    constuctor( type, color, width, height, fill )
     this.type = type;
     this.color = color;
+    this.width = width;
+    this.height = height;
+    this.fill = fill;
 
-    this.createShape = () => console.log (`This is my ${this.type}`)
+    this.createShape = () => console.log (`This is my ${this.type} ${this.color}`)
 }
 
 const circle = new Shape('circle', 'blue');
