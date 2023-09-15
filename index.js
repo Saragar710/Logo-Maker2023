d3fdf415f6c423af37aab07c2c39dcbc2760e646
Sara Garcia <saragca81@gmail.com>
2023-09-14 19:05:25 -0700

    added code

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ const questions = [
     {
         type: 'list',
         message: "Choose your shape.",
-        name: 'shape',
-        choices: ['Square','Triangle','Circle'],
-        
+        name: 'shapeType',
+        choices: ['square', 'triangle', 'circle'],
+
     },
     {
         type: 'confirm',
         name: "fontcolor",
         message: "Font color is white.",
-    
+
 
     }
-   
+
 ];
 // class Shapes {
 //     constructor(shape) {
@@ -38,61 +38,93 @@ const questions = [
 //         this.text = text;
 //     }
 
-    // render() {
-    //     if(this.shape === 'circle'){
-    //         return 'Render circle';
-    //     } else if (this.shape === 'triangle') {
-    //         return 'Render triangle';
-    //     } else if (this.shape === 'square') {
-    //         return 'Render square';
-    //     }else {
-    //         return 'Unknown shape';
-    //     }
+// render() {
+//     if(this.shape === 'circle'){
+//         return 'Render circle';
+//     } else if (this.shape === 'triangle') {
+//         return 'Render triangle';
+//     } else if (this.shape === 'square') {
+//         return 'Render square';
+//     }else {
+//         return 'Unknown shape';
+//     }
 
-    // }
+// }
 //}
-function writeToFile(fileName, data){
-    fs.writeFileSync(`./${fileName}` , data);
+function writeToFile(fileName, data) {
+    fs.writeFileSync(`./${fileName}`, data);
 }
 
 function init() {
-    inquirer 
-    .prompt(questions)
-    .then((answers) => {
-        console.log("results ...");
-        console.log(answers);
-
-        const shape = new Shapes(answers.shape);
-
-    
-        render();
-         //function render() {
-            if(this.shape === 'circle'){
-                return 'Render circle';
-            } else if (this.shape === 'triangle') {
-                return 'Render triangle';
-            } else if (this.shape === 'square') {
-                return 'Render square';
-            }else {
-                return 'Unknown shape';
+    run(){
+        return inquirer
+        .prompt([ 
+            {
+                type: 'input',
+                name: 'text',
+                message: "Enter three characters",
+            },
+            {
+                type: 'input',
+                name: 'color',
+                message: "Choose a color.",
+            },
+            {
+                type: 'list',
+                message: "Choose your shape.",
+                name: 'shapeType',
+                choices: ['square', 'triangle', 'circle'],
+        
+            },
+            {
+                type: 'confirm',
+                name: "fontcolor",
+                message: "Font color is white.",
+        
+        
             }
+        
+        ])
+        .then(({ text, color, fontColor, shapeType }) => {
+            // console.log("results ...");
+           
+            let shape;
+            switch (shapeType) {
+                case "square":
+                    shape = new Square();
+                    break;
+                case "triangle":
+                    shape = new Triangle();
+                    break; 
+                default: 
+                    shape = new Circle();
+                    break;
+    }
     
-      // }
-      
-     
-
-    //   if(shape.shape === "circle"){
-    //     console.log("You chose a circle.");
-    //     } else if (shape.shape === "triangle"){
-    //         console.log("You chose a triangle.");
-    //     } else if(shape.shape === "square") {
-    //         console.log("You chose a square");
-    //     }
-        writeToFile("logo.svg", shape.render())
-    })   
-}
+})
 
 
 
+            //function render() {
 
-init();
+          
+
+            // }
+
+
+
+            //   if(shape.shape === "circle"){
+            //     console.log("You chose a circle.");
+            //     } else if (shape.shape === "triangle"){
+            //         console.log("You chose a triangle.");
+            //     } else if(shape.shape === "square") {
+            //         console.log("You chose a square");
+            //     }
+//             writeToFile("logo.svg", shape.render())
+//         })
+// }
+
+
+
+
+init().run();
